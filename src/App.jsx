fe7ce8b598181830af9e66b00a23c590a79e0434
Jose Abraham <jabrahamocampo@users.noreply.github.com>
2025-08-05 22:28:50 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-       
+        <Route path="/" element={<Navigate to="/register" replace />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route
